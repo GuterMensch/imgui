590171f2bff3e7c236765a3abfaa6825cf6a205b
Thomas Conraths <thomas.conraths@rwth-aachen.de>
2019-05-07 20:57:12 +0200

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+-- premake file for imgui
+
+project "ImGui"
+	kind "StaticLib" -- static library
+	language "C++"
+	
+	targetdir ("../../../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../../../bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"imconfig.h",
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_widgets.cpp"
+	}
+	
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+		
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
